2e82b219307e3c6d15147ccab289968ae6836582
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-02-16 04:00:06 +0200

    cheerio each

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const requset = require("request");
 const cheerio = require("cheerio");
 
+// basics jquery
 requset("https://github.com/AbdulrahmanIsmailMohamed", (err, response, html) => {
     if (!err && response.statusCode == 200) {
         const $ = cheerio.load(html);
@@ -11,7 +12,14 @@ requset("https://github.com/AbdulrahmanIsmailMohamed", (err, response, html) => 
 
         // const output = siteHeading.find("h1").html();
         // const output = siteHeading.find("a").html();
-        const output = siteHeading.find("h1").text();
-        console.log(output);
+        // const output = siteHeading.find("h1").text();
+        // console.log(output);
+
+        // Query on popular repositry
+        $("ol").each((i, el) => {
+            const item = $(el).text();
+            const link = $(el).attr("href");
+            console.log(item);
+        });
     }
 })
